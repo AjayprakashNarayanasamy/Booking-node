a38f086d0af976a1b7a50573fbffef653ca173c3
AjayprakashNarayanasamy <ajayprakashn66@gmail.com>
2022-02-19 14:47:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongo = require("./shared/connect")
+const productRouter = require("./routes/products")
+const registerRouter = require("./routes/register")
+const app = express();
+
+app.use(express.json());
+mongo.connect()
+app.use("/product",productRouter)
+app.use("/register",registerRouter)
+app.listen(3000);
+
+
+
+//mongo.db.collection("Product").update(roomname:{req.body.roomname},${set:})
+
+//http://localhost:3000/register/signup => rooms already booked
+// _id
+// :
+// 620f2ec7aaab751512ddb19f
+// name
+// :
+// "AjayNew Data"
+// age
+// :
+// "231"
+// roomname
+// :
+// "rm2deluxe"
+//http://localhost:3000/register/gett => userdetails
+//http://localhost:3000/register/avail => available rooms
